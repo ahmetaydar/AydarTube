36b9787716c09b961c271097802b28103c743a4b
ahmetaydar <a.aydar2014@gmail.com>
2023-07-10 18:29:37 +0300

    create app ui

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Main = styled.div`
+  flex: 7;
+`;
+const Wrapper = styled.div``;
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>video cards</Wrapper>
+      </Main>
+    </Container>
+  );
 }
 
 export default App;
